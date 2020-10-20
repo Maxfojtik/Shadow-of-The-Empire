016bd5574c220be5723bfcbb4003f572b0e532e9
Maxfojtik <maxfojtik@bex.net>
2020-10-20 16:19:39 -0400

    VotingPopuated

--- Webpage/backend.js
+++ Webpage/backend.js
@@ -64,6 +64,10 @@ class BackendConnection {
 		{
 			votedFor(Integer.parseInt(params[1]), Integer.parseInt(params[2]), params[3])//problem number, solution number, names
 		}
+		if(params[0]=="PopulateUserVotingPhase")
+		{
+			populateUserVotingPhase(params[1]);
+		}
 	}
 
 	onError(evt) {
